f9a3a0f27ebde1a218e20997d87badaf7dfdd3b8
Puneet Bansal <puneetbansal15801@gmail.com>
2021-10-18 22:05:34 +0530

    login actions

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,66 @@
+import * as ActionTypes from "./actionTypes"
+
+export const requestLogin = (creds) => {
+    return{
+        type: ActionTypes.LOGIN_REQUEST,
+        creds
+    }
+}
+
+export const receiveLogin = (response) => {
+    return {
+        type: ActionTypes.LOGIN_SUCCESS,
+        token: response.token,
+        admin: response.admin
+    }
+}
+
+export const loginError = (message) => {
+    return {
+        type: ActionTypes.LOGIN_FAILURE,
+        message
+    }
+}
+
+export const LoginUser = (creds) => (dispatch) => {
+    dispatch(requestLogin(creds));
+    return fetch(process.env.REACT_BASEURL + 'users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(creds)
+    })
+        .then(response => {
+            if(response.ok){
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                throw error;
+            })
+        .then(response => response.json())
+        .then(response => {
+            if (response.success) {
+                // If login was successful, set the token in local storage
+                localStorage.setItem('token', response.token);
+                localStorage.setItem('creds', JSON.stringify(creds));
+                localStorage.setItem('admin', response.admin);
+                // Dispatch the success action
+                dispatch(receiveLogin(response));
+            }
+            else {
+                var error = new Error('Error ' + response.status);
+                error.response = response;
+                throw error;
+            }
+        })
+        .catch(error => dispatch(loginError(error.message)))
+}
+
+
+
---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,36 @@
+import * as ActionTypes from "../actions/actionTypes";
+
+export const Auth = (state = {
+    isLoading: false,
+    isAuthenticated: localStorage.getItem('token') ? true : false,
+    token: localStorage.getItem('token'),
+    user: localStorage.getItem('creds') ? JSON.parse(localStorage.getItem('creds')) : null,
+    errMessage: null,
+    admin: false
+}, action) => {
+    switch(action.type) {
+        case ActionTypes.LOGIN_REQUEST:
+            return{
+                ...state,
+                isLoading: true,
+                isAuthenticated: false,
+                user: action.creds
+            };
+        case ActionTypes.LOGIN_FAILURE:
+            return{
+                ...state,
+                isLoading: false,
+                isAuthenticated: false,
+                errMessage: action.message
+            };
+        case ActionTypes.LOGIN_SUCCESS:
+            return{
+                ...state,
+                isLoading: false,
+                isAuthenticated: true,
+                errMessage: '',
+                token: action.token,
+                admin: action.admin
+            };
+    }
+}
