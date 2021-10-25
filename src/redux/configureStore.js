0b00bdf9df0bda5523a392dc05f7e04144545445
Puneet Bansal <puneetbansal15801@gmail.com>
2021-10-25 12:53:17 +0530

    add and delete notice actions

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Auth } from './reducers/auth';
+import { Notices } from "./reducers/notices";
 import thunk from 'redux-thunk';
 
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             auth: Auth,
+            notices: Notices,
         }, +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
         applyMiddleware(thunk)
     );
